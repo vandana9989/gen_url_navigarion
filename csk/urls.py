9e164262e70466afbc2173203221d805f06708da
vandana9989 <vandanaavula020@gmail.com>
2023-06-01 21:09:22 +0530

    first commit

---
+++ csk/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from csk.views import *
+app_name = 'csk'
+
+urlpatterns=[
+    path('dhoni/',dhoni,name='dhoni'),
+   
+
+]
---
+++ rcb/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rcb.views import *
+app_name = 'rcb'
+
+urlpatterns=[
+    path('virat/',virat,name='virat')
+  
+   
+
+]
